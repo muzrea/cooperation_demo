fcbc8752b41e6c0f1040c56f32d7f2bacd3e557e
Layla Wang <wanglei0717@outlook.com>
2019-04-29 10:00:26 +0800

    remove index and add sql of creating database and tables

---
+++ sql/create_examination_sys_database.sql
@@ -0,0 +1,55 @@
+CREATE DATABASE examination_management_sys DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+USE examination_management_sys;
+
+CREATE TABLE user (
+	id INT AUTO_INCREMENT,
+    username VARCHAR(20) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    role_id INT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET= utf8;
+
+CREATE TABLE role (
+	id INT AUTO_INCREMENT,
+    name VARCHAR(20) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8; 
+
+CREATE TABLE profile (
+	id INT AUTO_INCREMENT,
+    name VARCHAR(50) NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE role_profile (
+	id INT AUTO_INCREMENT,
+    role_id INT,
+    profile_id INT,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE examination (
+	id INT,
+    name VARCHAR(20) NOT NULL,
+    time INT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE subject_teacher (
+	id INT AUTO_INCREMENT,
+    subject_id INT NOT NULL,
+    teacher_id INT NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+CREATE TABLE score (
+	id INT AUTO_INCREMENT,
+    student_id INT NOT NULL,
+    subject_id INT NOT NULL,
+    score DOUBLE NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8;
+
+
+
+
